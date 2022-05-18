9dfc7eaf55a621f1da51c1b797ddb9739c63336f
Alessandro231 <you@example.com>
2022-05-18 12:40:45 -0500

    Se crea el componente encabezado

--- src/Componentes_generales.jsx
+++ src/Componentes_generales.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Formulario } from './Formulario';
-import { Tabla } from './Tabla';
+import React from "react";
+import { Encabezado } from "./Encabezado";
+import { Formulario } from "./Formulario";
+import { Tabla } from "./Tabla";
 
-
-export function Aplicacion(){
-    return(
-        <React.Fragment>
-            
-            <Formulario/>
-            <Tabla/>
-            
-        </React.Fragment>
-    )
+export function Aplicacion() {
+  return (
+    <React.Fragment>
+      <Encabezado />
+      <Formulario />
+      <Tabla />
+    </React.Fragment>
+  );
 }
--- src/Tabla.jsx
+++ src/Tabla.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 
-export function Tabla(){
-    return(
-    <table>
+export function Tabla() {
+  return (
+    <table class="table">
+      <thead>
         <tr>
-
-            <td>Celda 1</td>
-
-            <td>Celda 2</td>
-
-            <td>Celda 3</td>
-
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
         </tr>
-
+      </thead>
+      <tbody>
         <tr>
-
-            <td>Celda 4</td>
-
-            <td>Celda 5</td>
-
-            <td>Celda 6</td>
-
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
         </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
     </table>
-    )
+  );
 }
