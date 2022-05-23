2e126181161f3f98884427662f65cce4a12f764f
Alessandro231 <aaristizabal@unal.edu.co>
2022-05-22 23:14:26 -0500

    Se termina de crear el componente formulario y encabezado

--- src/Encabezado.jsx
+++ src/Encabezado.jsx
@@ -2,27 +2,37 @@ import React from "react";
 
 export function Encabezado() {
   return (
-<nav class="nav">
-    <ul className="nav justify-content-end">
-      <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">
-          Active
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link">Disabled</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">Next generation components</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Hardware interno</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Hardware externo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Mantenimiento</a>
+        </li>
+      </ul>
+      <nav class="navbar bg-light">
+    <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"></img>
+    </a>
+    </div>
+      </nav>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Busqueda de producto" aria-label="Search"></input>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
 </nav>
   )
 }
--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -2,12 +2,47 @@ import React from 'react';
 
 export function Formulario() {
   return (
-    <form>
-        <div>
-            <label for="exampleInputEmail">Correo</label>
-            <input type="email" id="exampleInput"/>
-            <button type="submit">Enviar</button>
-        </div>
-    </form>
+    <form class="row g-3">
+  <div class="col-auto">
+    <label for="staticEmail2" class="visually-hidden">Email</label>
+    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com"></input>
+  </div>
+  <div class="col-auto">
+    <label for="inputPassword2" class="visually-hidden">Password</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Contrasena"></input>
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Ingresar</button>
+  </div>
+  <label for="exampleDataList" class="form-label">Departamento de residencia</label>
+<input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..."></input>
+<datalist id="datalistOptions">
+  <option value="San Francisco"></option>
+  <option value="New York"></option>
+  <option value="Seattle"></option>
+  <option value="Los Angeles"></option>
+  <option value="Chicago"></option>
+</datalist>
+  <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <span class="navbar-text">
+      Desea recibir ofertas en su correo electronico?
+    </span>
+  </div>
+  </nav>
+  <div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+  <label class="form-check-label" for="flexRadioDefault1">
+    Si
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+  <label class="form-check-label" for="flexRadioDefault2">
+    No
+  </label>
+</div>
+</form>
+
   )
 }
